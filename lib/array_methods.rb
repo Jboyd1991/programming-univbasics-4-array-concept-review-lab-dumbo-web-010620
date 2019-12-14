58dc966928f618c24c400fef5462036cfbecf76e
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-14 02:39:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,9 @@ def find_max_value(array)
   max = array[0]
   array.length.times { |index| 
 if array[index] > max 
-  max = array[index] } 
+  max = array[index] 
+end }
+return max 
 end
 
 def find_min_value(array)
